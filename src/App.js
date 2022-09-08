931898e2d2db21e2f4fc85b04c41a24eec618cc7
SvetlanaGaliuk <sgal3535@gmail.com>
2022-09-09 02:22:44 +0300

    Added structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Svetlana Galiuk and is{" "}
-        <a
-          href="https://github.com/SvetlanaGaliuk/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-soursed on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by Svetlana Galiuk and is{" "}
+          <a
+            href="https://github.com/SvetlanaGaliuk/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-soursed on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
